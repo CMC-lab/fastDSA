8860cfe084265975aff4edba47ca78ae7cfc61da
Arman.behi <Arman.behi@gmail.com>
2025-02-24 10:46:50 +0100

    changing names

---
+++ src/__init__.py
@@ -0,0 +1,5 @@
+from fastDSA.fastDSA import fastDSA
+from fastDSA.dmd import DMD
+from fastDSA.kerneldmd import KernelDMD
+from fastDSA.simdist import SimilarityTransformDist
+from fastDSA.stats import *
